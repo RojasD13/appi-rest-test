991270700d9d6ae8c626c2c4945d0e5f23498aff
DanielRojasDEV <daniel.rojas13@uptc.edu.co>
2024-09-24 17:05:20 -0500

    first commit

---
+++ src/test/java/com/edu/uptc/api_rest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
